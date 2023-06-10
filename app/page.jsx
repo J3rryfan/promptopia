2d7527491a58e3e2645ae5b900adbf5f6049bdf0
Jerry Fan <Jerryfan71@gmail.com>
2023-06-09 23:06:45 -0700

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <section className="flex flex-center flex-col w-full">
+      <h1 className="head_text text center">Discover & Share
+        <br className="max-md:hidden" />
+        <span className="text-center orange_gradient "> AI-Powered Prompts</span>
+      </h1>
+      <p className="text-center desc">Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
+
+    </section>
+  );
+};
+
+export default Home;
