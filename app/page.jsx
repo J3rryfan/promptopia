3a7cfb8faef8b4f0d8216c212a65c3ec8699a820
Jerry Fan <Jerryfan71@gmail.com>
2023-06-10 01:16:04 -0700

    finish sign up

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
   return (
     <section className="flex flex-center flex-col w-full">
@@ -7,6 +9,7 @@ const Home = () => {
       </h1>
       <p className="text-center desc">Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts</p>
 
+      <Feed />
     </section>
   );
 };
